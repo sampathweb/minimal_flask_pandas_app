7a8127a2bc4b60500ba8968875a74c19b6edf148
Ramesh Sampath <ramesh@sampathweb.com>
2016-05-19 09:59:44 -0700

    Update views

--- app/views.py
+++ app/views.py
@@ -21,12 +21,17 @@ def index():
     temp_plt_file = tempfile.NamedTemporaryFile(
         dir=TEMP_PLOT_DIR, suffix='.png',delete=False)
     ax.get_figure().savefig(temp_plt_file)
-    # get the file's name (rather than the whole path - the template will need that)
+    # get the file's name (rather than the whole path
     plot_png = temp_plt_file.name.split('/')[-1]
 
-    # mpld3_data = mpld3.fig_to_dict(ax.get_figure())
-    custom_format = lambda x: '<span class="significant"><bold>%f</bold></span>' % x
-    table_html = df.head(20).reset_index().to_html(classes=['u-full-width'],
-        formatters={'actual': custom_format}, index=False).replace('border="1"','border="0"')
+    table_html = (df.head(20)
+                    .reset_index()
+                    .to_html(classes=['u-full-width'], index=False)
+                    .replace('border="1"','border="0"')
+                )
+
+    # custom_format = lambda x: '<span class="significant"><bold>%f</bold></span>' % x
+    # table_html = df.head(20).reset_index().to_html(classes=['u-full-width'],
+    #     formatters={'actual': custom_format}, index=False).replace('border="1"','border="0"')
     return render_template('index.html',
         data_table=table_html, plot_png=plot_png)
